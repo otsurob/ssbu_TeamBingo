4b46b5604626880d9c421e3973356803082431d6
otsurob <kzkokm0618@gmail.com>
2026-01-09 02:52:11 +0900

    player_leftのdeleteしてからgetしてた問題の解消

--- server/usecase/room_usecase.go
+++ server/usecase/room_usecase.go
@@ -268,15 +268,15 @@ func (ru *roomUsecase) DeletePlayer(roomName string) error {
 }
 
 func (ru *roomUsecase) DeleteOnePlayer(roomName string, name string) error {
+	var leftPlayer domain.Player
+	if err := ru.rr.GetPlayer(&leftPlayer, roomName, name); err != nil {
+		return err
+	}
 	if err := ru.rr.DeleteOnePlayer(roomName, name); err != nil {
 		return err
 	}
 	if ru.ep != nil {
-		// 退出したプレイヤーの情報を取得してからイベント送信
-		var leftPlayer domain.Player
-		if err := ru.rr.GetPlayer(&leftPlayer, roomName, name); err == nil {
-			_ = ru.ep.PushPlayerLeft(roomName, leftPlayer)
-		}
+		_ = ru.ep.PushPlayerLeft(roomName, leftPlayer)
 	}
 	return nil
 }
